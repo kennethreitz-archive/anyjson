f83cf5c347419aa599b80f7e9ec65916cbf5453b
Rune Halvorsen <runefh@gmail.com>
2009-06-20 22:27:58 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ author, email = anyjson.__author__[:-1].split(' <')
 setup(name='anyjson',
       version=anyjson.__version__,
       description=anyjson.__doc__,
-      long_description="""""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+            'License :: OSI Approved :: BSD License',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python'
+            ],
       keywords='json',
       author=author,
       author_email=email,
       url='http://bitbucket.org/runeh/anyjson',
       license='BSD',
-      modules=["anyjson.py"],
-      include_package_data=True,
-      zip_safe=False
-      )
+      py_modules=['anyjson'],
+)
